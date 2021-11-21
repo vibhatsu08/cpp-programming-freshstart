c3289c3208062ae02809a4ef1c37b83b54e4eb75
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-11-21 16:16:18 +0530

    c++ variables and literals

---
+++ Variables/Variables/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Variables
+//
+//  Created by Vedant Mistry on 21/11/21.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age1 = 18; //this is an integer
+    cout << age1 << "\n";
+    float age2 = 18.18;
+    cout << age2 << "\n";
+    const int age = 19;
+    cout << age;
+    
+    
+}
