aa24c93db9d33f41f483bde697ee2a15f00a2eb0
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-12-03 20:11:36 +0530

    program to find the length of the given string

---
+++ findLengthOfString/findLengthOfString/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    string str;
+    getline(cin, str);
+    
+    cout << "Length of the given string is: " << str.size() << endl;
+    
+    return 0;
+}
