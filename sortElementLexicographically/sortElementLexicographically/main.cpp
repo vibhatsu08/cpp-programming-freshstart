2c9d8447daaee36ebce175de164a3ed182f2c956
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-12-05 00:18:37 +0530

    Add files via upload

--- sortElementLexicographically/sortElementLexicographically/main.cpp
+++ sortElementLexicographically/sortElementLexicographically/main.cpp
@@ -1,25 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
+
+int main (){
     char str[200], newString;
-    long length;
-    cout<<"Enter the String: ";
     gets(str);
-    length = strlen(str);
-    for(int i=0; i<length; i++)
-    {
-        for(int j=0; j<(length-1); j++)
-        {
-            if(str[j]>str[j+1])
-            {
+    long length = strlen(str);
+    
+    for (int i=0; i<length; i++){
+        for (int j=0; j<(length-1); j++){
+            if (str[j] > str[j + 1]){
                 newString = str[j];
-                str[j] = str[j+1];
-                str[j+1] = newString;
+                str[j] = str[j + 1];
+                str[j + 1] = newString;
             }
         }
     }
-    cout<<"\nString in Ascending Order: "<<str;
-    cout<<endl;
+    
+    cout << str << endl;
+    
     return 0;
 }
