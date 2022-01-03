3b78f115e2025c6d794fa855723b0071566d0899
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2022-01-04 01:34:28 +0530

    print address of variables

---
+++ pointersPrintVariableAddresses/pointersPrintVariableAddresses/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int var1 = 1;
+    int var2 = 2;
+    int var3 = 3;
+    
+    cout << "Variable address of " << var1 << " is " << &var1 << endl;
+    cout << "Variable address of " << var2 << " is " << &var2 << endl;
+    cout << "Variable address of " << var3 << " is " << &var3 << endl;
+    
+    return 0;
+}
